02824ddf6450b7dd02169a73e7f4f8e7e5d2640f
Odumosu <h.azeez@hotmail.co.uk>
2020-12-20 16:25:35 +0000

    Remove migrations from gitignore
    So that Github can publish to azure when  commit to master

---
+++ Migrations/20201102225040_AddSpecialServiceColumns.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CheckinPPP.Migrations
+{
+    public partial class AddSpecialServiceColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSpecialService",
+                table: "Bookings",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SpecialServiceName",
+                table: "Bookings",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSpecialService",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "SpecialServiceName",
+                table: "Bookings");
+        }
+    }
+}
---
+++ Migrations/20201220154227_SpecialServiceYoutubeUrl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CheckinPPP.Migrations
+{
+    public partial class SpecialServiceYoutubeUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SpecialServiceYoutubeUrl",
+                table: "Bookings",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SpecialServiceYoutubeUrl",
+                table: "Bookings");
+        }
+    }
+}
